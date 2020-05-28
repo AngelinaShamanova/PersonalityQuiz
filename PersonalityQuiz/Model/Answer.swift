e75bb8599b804ab8a692c4130c5dbe31e61306ca
Angelina Shamanova <angelinashamanova@gmail.com>
2020-05-28 13:35:30 +0300

    7. Create models

---
+++ PersonalityQuiz/Model/Answer.swift
@@ -0,0 +1,12 @@
+//
+//  Answer.swift
+//  PersonalityQuiz
+//
+//  Created by MacBook on 28/05/2020.
+//  Copyright © 2020 mybestcompany.com. All rights reserved.
+//
+
+struct Answer {
+    var text: String
+    var type: AnimalType
+}
---
+++ PersonalityQuiz/Model/ResponseType.swift
@@ -0,0 +1,13 @@
+//
+//  ResponceType.swift
+//  PersonalityQuiz
+//
+//  Created by MacBook on 28/05/2020.
+//  Copyright © 2020 mybestcompany.com. All rights reserved.
+//
+
+enum ResponseType {
+    case single
+    case multiple
+    case ranged
+}
